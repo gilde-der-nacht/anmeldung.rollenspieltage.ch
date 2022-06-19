06954228df886579304fe35bd4bd378ec1306389
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2022-06-19 11:19:00 +0000

    move head to layout

--- components/layout.js
+++ components/layout.js
@@ -1,10 +1,39 @@
 import Header from '../components/header'
 import Footer from '../components/footer'
-
+import Head from "next/head";
+import Script from 'next/script'
 
 export default function Layout({ children }) {
     return (
         <>
+            <Head>
+                <title>Luzerner Rollenspieltage</title>
+                <Script src="https://test.rollenspieltage.ch/icons/duotone.min.js"></Script>
+                <Script src="https://test.rollenspieltage.ch/icons/fontawesome.min.js"></Script>
+                <link
+                    rel="preload"
+                    as="font"
+                    crossOrigin="anonymous"
+                    href="https://test.rollenspieltage.ch/fonts/krona-one-normal-400.woff2"
+                    type="font/woff2"
+                />
+                <link
+                    rel="preload"
+                    as="font"
+                    crossOrigin="anonymous"
+                    href="https://test.rollenspieltage.ch/fonts/dm-sans-normal-400.woff2"
+                    type="font/woff2"
+                />
+                <link
+                    rel="shortcut icon"
+                    href="https://test.rollenspieltage.ch/favicon.ico"
+                    type="image/x-icon"
+                />
+                <link
+                    rel="stylesheet"
+                    href="https://test.rollenspieltage.ch/style/style.css"
+                />
+            </Head>
             <Header />
             <main className="main-content content">
                 {children}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
 import type { NextPage } from "next";
 import Layout from "../components/layout";
-import Head from "next/head";
-import Script from 'next/script'
 import { useEffect } from "react";
 
 const Home: NextPage = () => {
@@ -9,40 +7,9 @@ const Home: NextPage = () => {
     document?.querySelector("body")?.classList.add("rst");
   });
   return (
-    <div>
-      <Head>
-        <title>Luzerner Rollenspieltage</title>
-        <Script src="https://test.rollenspieltage.ch/icons/duotone.min.js"></Script>
-        <Script src="https://test.rollenspieltage.ch/icons/fontawesome.min.js"></Script>
-        <link
-          rel="preload"
-          as="font"
-          crossOrigin="anonymous"
-          href="https://test.rollenspieltage.ch/fonts/krona-one-normal-400.woff2"
-          type="font/woff2"
-        />
-        <link
-          rel="preload"
-          as="font"
-          crossOrigin="anonymous"
-          href="https://test.rollenspieltage.ch/fonts/dm-sans-normal-400.woff2"
-          type="font/woff2"
-        />
-        <link
-          rel="shortcut icon"
-          href="https://test.rollenspieltage.ch/favicon.ico"
-          type="image/x-icon"
-        />
-        <link
-          rel="stylesheet"
-          href="https://test.rollenspieltage.ch/style/style.css"
-        />
-      </Head>
-
-      <Layout>
-        <h1>Anmeldung</h1>
-      </Layout>
-    </div>
+    <Layout>
+      <h1>Anmeldung</h1>
+    </Layout>
   );
 };
 

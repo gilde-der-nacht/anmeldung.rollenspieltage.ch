18e7a84feb892bf20d0f986ef478cb1b9b449ac0
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2022-06-30 15:49:30 +0000

    Remove `test.`

--- components/general/Layout.tsx
+++ components/general/Layout.tsx
@@ -13,30 +13,30 @@ export default function Layout({ children }: { children: JSX.Element }) {
     <>
       <Head>
         <title>Anmeldung | Luzerner Rollenspieltage</title>
-        <Script src="https://test.rollenspieltage.ch/icons/duotone.min.js"></Script>
-        <Script src="https://test.rollenspieltage.ch/icons/fontawesome.min.js"></Script>
+        <Script src="https://rollenspieltage.ch/icons/duotone.min.js"></Script>
+        <Script src="https://rollenspieltage.ch/icons/fontawesome.min.js"></Script>
         <link
           rel="preload"
           as="font"
           crossOrigin="anonymous"
-          href="https://test.rollenspieltage.ch/fonts/krona-one-normal-400.woff2"
+          href="https://rollenspieltage.ch/fonts/krona-one-normal-400.woff2"
           type="font/woff2"
         />
         <link
           rel="preload"
           as="font"
           crossOrigin="anonymous"
-          href="https://test.rollenspieltage.ch/fonts/dm-sans-normal-400.woff2"
+          href="https://rollenspieltage.ch/fonts/dm-sans-normal-400.woff2"
           type="font/woff2"
         />
         <link
           rel="shortcut icon"
-          href="https://test.rollenspieltage.ch/favicon.ico"
+          href="https://rollenspieltage.ch/favicon.ico"
           type="image/x-icon"
         />
         <link
           rel="stylesheet"
-          href="https://test.rollenspieltage.ch/style/style.css"
+          href="https://rollenspieltage.ch/style/style.css"
         />
       </Head>
       <Header />

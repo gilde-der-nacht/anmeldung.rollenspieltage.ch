ab6ee55ff8b107d52da291193044c698ccce6cb2
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2022-07-09 09:12:31 +0000

    Refactor game round edit form

---
+++ components/GameRoundEdit.tsx
@@ -0,0 +1,222 @@
+import Router from "next/router";
+import { useEffect, useState } from "react";
+import { ButtonWithEvent } from "./form/ButtonWithEvent";
+import { RadioButtons } from "./form/RadioButtons";
+import { TextInput } from "./form/TextInput";
+import { GameRound } from "./GameRoundStore";
+import { AlertBox } from "./general/AlertBox";
+import DeleteIcon from "./icons/DeleteIcon";
+
+type FormProps = {
+  gameRound: GameRound;
+  onSubmit: (gameRound: GameRound) => void;
+  onDelete?: (gameRound: GameRound) => void;
+};
+
+export const GameRoundEdit = ({ gameRound, onSubmit, onDelete }: FormProps) => {
+  const [id, setId] = useState("");
+  const [name, setName] = useState("");
+  const [duration, setDuration] = useState(120);
+  const [minPlayerCount, setMinPlayerCount] = useState(3);
+  const [maxPlayerCount, setMaxPlayerCount] = useState(4);
+  const [repetition, setRepetition] = useState({
+    name: "repetition",
+    currentValue: 0,
+    options: [
+      {
+        label: "einmalig",
+        value: 0,
+      },
+      {
+        label: "einmal pro Tag",
+        value: 1,
+      },
+      {
+        label: "gerne auch mehrmals",
+        value: 2,
+      },
+    ],
+  });
+
+  useEffect(() => {
+    setId(gameRound.id);
+    setName(gameRound.name);
+    setDuration(gameRound.duration);
+    setMinPlayerCount(gameRound.minPlayerCount);
+    setMaxPlayerCount(gameRound.maxPlayerCount);
+    updateRepetitionValue(gameRound.repetition);
+  }, [gameRound]);
+
+  const [errorWithName, setErrorWithName] = useState(false);
+  const [errorWithDuration, setErrorWithDuration] = useState(false);
+  const [errorWithPlayerCount, setErrorWithPlayerCount] = useState(false);
+  const [generalError, setGeneralError] = useState(false);
+
+  useEffect(() => {
+    setErrorWithName(name.trim().length < 1);
+  }, [name]);
+
+  useEffect(() => {
+    setErrorWithDuration(duration < 15 || duration > 180);
+  }, [duration]);
+
+  useEffect(() => {
+    setErrorWithPlayerCount(
+      minPlayerCount < 0 || minPlayerCount > maxPlayerCount
+    );
+  }, [minPlayerCount, maxPlayerCount]);
+
+  useEffect(() => {
+    setGeneralError(false);
+  }, [name, duration, minPlayerCount, maxPlayerCount]);
+
+  const updateRepetitionValue = (num: number) => {
+    setRepetition((currVal) => {
+      return { ...currVal, currentValue: num };
+    });
+  };
+
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    if (name.trim().length < 1) {
+      setGeneralError(true);
+      return;
+    }
+    if (duration < 15) {
+      setGeneralError(true);
+      return;
+    }
+    if (duration > 180) {
+      setGeneralError(true);
+      return;
+    }
+    if (minPlayerCount < 0) {
+      setGeneralError(true);
+      return;
+    }
+    if (minPlayerCount > maxPlayerCount) {
+      setGeneralError(true);
+      return;
+    }
+    onSubmit({
+      id,
+      name: name.trim(),
+      duration,
+      minPlayerCount,
+      maxPlayerCount,
+      repetition: repetition.currentValue,
+      active: true,
+    });
+
+    Router.push("/");
+  };
+
+  const handleDelete = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    if (typeof onDelete === "undefined") {
+      return;
+    }
+    onDelete({
+      id,
+      name: name.trim(),
+      duration,
+      minPlayerCount,
+      maxPlayerCount,
+      repetition: repetition.currentValue,
+      active: false,
+    });
+    Router.push("/");
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="content">
+      <TextInput
+        state={name}
+        setter={setName}
+        label="Name der Spielrunde"
+        placeholder="Name der Spielrunde"
+        clue="z.B. Name des Abenteuers, Name des Spielsystems"
+      />
+      {errorWithName && (
+        <div>
+          <AlertBox>
+            <p>Wähle bitte gültige Namen für die Spielrunde.</p>
+          </AlertBox>
+        </div>
+      )}
+      <TextInput
+        state={duration}
+        setter={(val) => setDuration(parseInt(val))}
+        type="number"
+        label="Dauer der Spielrunde in Minuten"
+        placeholder="Dauer der Spielrunde"
+        clue="max. 3 Stunden, resp. 180 Minuten"
+      />
+      {errorWithDuration && (
+        <div>
+          <AlertBox>
+            <p>
+              Wähle bitte eine gültige Minutenanzahl für die Spielrunde
+              (mindestens 15 Minuten und maximal 3 Stunden).
+            </p>
+          </AlertBox>
+        </div>
+      )}
+      <TextInput
+        state={minPlayerCount}
+        setter={(val) => setMinPlayerCount(parseInt(val))}
+        type="number"
+        label="Minimale Spieleranzahl"
+        placeholder="Minimale Spieleranzahl"
+      />
+      {errorWithPlayerCount && (
+        <div>
+          <AlertBox>
+            <p>Wähle bitte gültige Zahlen für die Spieleranzahl aus.</p>
+          </AlertBox>
+        </div>
+      )}
+      <TextInput
+        state={maxPlayerCount}
+        setter={(val) => setMaxPlayerCount(parseInt(val))}
+        type="number"
+        label="Maximale Spieleranzahl"
+        placeholder="Maximale Spieleranzahl"
+      />
+      <h5>Repetition</h5>
+      <p>Wie oft bist du bereit diese Runde durchzuführen?</p>
+      <RadioButtons state={repetition} setter={updateRepetitionValue} />
+      {generalError && (
+        <div>
+          <AlertBox>
+            <p>
+              Leider ist die Spielrunde nicht gültig. Überprüfe nochmals alle
+              Felder.
+            </p>
+          </AlertBox>
+        </div>
+      )}
+
+      <div style={{ display: "flex", gap: "1rem", flexWrap: "wrap" }}>
+        {typeof onDelete === "undefined" ? (
+          <ButtonWithEvent isSubmit={true}>
+            <span> Neue Spielrunde erstellen </span>
+          </ButtonWithEvent>
+        ) : (
+          <>
+            <ButtonWithEvent isSubmit={true}>
+              <span> Änderungen speichern </span>
+            </ButtonWithEvent>
+            <ButtonWithEvent
+              event={handleDelete}
+              icon={<DeleteIcon />}
+              type="danger"
+            >
+              <span> Spielrunde löschen </span>
+            </ButtonWithEvent>
+          </>
+        )}
+      </div>
+    </form>
+  );
+};
--- pages/neue-spielrunde.tsx
+++ pages/neue-spielrunde.tsx
@@ -1,15 +1,10 @@
 import { NextPage } from "next";
-import Router from "next/router";
-import React, { useEffect, useState } from "react";
-import { ButtonWithEvent } from "../components/form/ButtonWithEvent";
-import { RadioButtons } from "../components/form/RadioButtons";
-import { TextInput } from "../components/form/TextInput";
+import { GameRoundEdit } from "../components/GameRoundEdit";
 import {
   addGameRound,
   GameRound,
-  getRandomId,
+  getRandomId
 } from "../components/GameRoundStore";
-import { AlertBox } from "../components/general/AlertBox";
 import { useLocalStorage } from "../components/general/store";
 
 const Spielleiten: NextPage = () => {
@@ -17,96 +12,6 @@ const Spielleiten: NextPage = () => {
     "gameRounds",
     []
   );
-  const [name, setName] = useState("");
-  const [duration, setDuration] = useState(120);
-  const [minPlayerCount, setMinPlayerCount] = useState(3);
-  const [maxPlayerCount, setMaxPlayerCount] = useState(4);
-  const [repetition, setRepetition] = useState({
-    name: "repetition",
-    currentValue: 0,
-    options: [
-      {
-        label: "einmalig",
-        value: 0,
-      },
-      {
-        label: "einmal pro Tag",
-        value: 1,
-      },
-      {
-        label: "gerne auch mehrmals",
-        value: 2,
-      },
-    ],
-  });
-
-  const [errorWithName, setErrorWithName] = useState(false);
-  const [errorWithDuration, setErrorWithDuration] = useState(false);
-  const [errorWithPlayerCount, setErrorWithPlayerCount] = useState(false);
-  const [generalError, setGeneralError] = useState(false);
-
-  useEffect(() => {
-    setErrorWithName(name.trim().length < 1);
-  }, [name]);
-
-  useEffect(() => {
-    setErrorWithDuration(duration < 15 || duration > 180);
-  }, [duration]);
-
-  useEffect(() => {
-    setErrorWithPlayerCount(
-      minPlayerCount < 0 || minPlayerCount > maxPlayerCount
-    );
-  }, [minPlayerCount, maxPlayerCount]);
-
-  useEffect(() => {
-    setGeneralError(false);
-  }, [name, duration, minPlayerCount, maxPlayerCount]);
-
-  const updateRepetitionValue = (num: number) => {
-    setRepetition((currVal) => {
-      return { ...currVal, currentValue: num };
-    });
-  };
-
-  const handleSubmit = (e: React.SyntheticEvent) => {
-    e.preventDefault();
-    if (name.trim().length < 1) {
-      setGeneralError(true);
-      return;
-    }
-    if (duration < 15) {
-      setGeneralError(true);
-      return;
-    }
-    if (duration > 180) {
-      setGeneralError(true);
-      return;
-    }
-    if (minPlayerCount < 0) {
-      setGeneralError(true);
-      return;
-    }
-    if (minPlayerCount > maxPlayerCount) {
-      setGeneralError(true);
-      return;
-    }
-
-    addGameRound(
-      {
-        id: getRandomId(),
-        name: name.trim(),
-        duration,
-        minPlayerCount,
-        maxPlayerCount,
-        repetition: repetition.currentValue,
-        active: true,
-      },
-      setGameRounds
-    );
-
-    Router.push("/");
-  };
 
   return (
     <>
@@ -122,77 +27,18 @@ const Spielleiten: NextPage = () => {
         Spielmaterial für deine Spieler:innen mitnimmst. Für Schreibzeug sorgen
         wir.
       </p>
-      <form onSubmit={handleSubmit} className="content">
-        <TextInput
-          state={name}
-          setter={setName}
-          label="Name der Spielrunde"
-          placeholder="Name der Spielrunde"
-          clue="z.B. Name des Abenteuers, Name des Spielsystems"
-        />
-        {errorWithName && (
-          <div>
-            <AlertBox>
-              <p>Wähle bitte gültige Namen für die Spielrunde.</p>
-            </AlertBox>
-          </div>
-        )}
-        <TextInput
-          state={duration}
-          setter={(val) => setDuration(parseInt(val))}
-          type="number"
-          label="Dauer der Spielrunde in Minuten"
-          placeholder="Dauer der Spielrunde"
-          clue="max. 3 Stunden, resp. 180 Minuten"
-        />
-        {errorWithDuration && (
-          <div>
-            <AlertBox>
-              <p>
-                Wähle bitte eine gültige Minutenanzahl für die Spielrunde
-                (mindestens 15 Minuten und maximal 3 Stunden).
-              </p>
-            </AlertBox>
-          </div>
-        )}
-        <TextInput
-          state={minPlayerCount}
-          setter={(val) => setMinPlayerCount(parseInt(val))}
-          type="number"
-          label="Minimale Spieleranzahl"
-          placeholder="Minimale Spieleranzahl"
-        />
-        {errorWithPlayerCount && (
-          <div>
-            <AlertBox>
-              <p>Wähle bitte gültige Zahlen für die Spieleranzahl aus.</p>
-            </AlertBox>
-          </div>
-        )}
-        <TextInput
-          state={maxPlayerCount}
-          setter={(val) => setMaxPlayerCount(parseInt(val))}
-          type="number"
-          label="Maximale Spieleranzahl"
-          placeholder="Maximale Spieleranzahl"
-        />
-        <h5>Repetition</h5>
-        <p>Wie oft bist du bereit diese Runde durchzuführen?</p>
-        <RadioButtons state={repetition} setter={updateRepetitionValue} />
-        {generalError && (
-          <div>
-            <AlertBox>
-              <p>
-                Leider ist die Spielrunde nicht gültig. Überprüfe nochmals alle
-                Felder.
-              </p>
-            </AlertBox>
-          </div>
-        )}
-        <ButtonWithEvent isSubmit={true}>
-          <span> Neue Spielrunde erstellen </span>
-        </ButtonWithEvent>
-      </form>
+      <GameRoundEdit
+        gameRound={{
+          id: getRandomId(),
+          name: "",
+          duration: 120,
+          minPlayerCount: 3,
+          maxPlayerCount: 4,
+          repetition: 0,
+          active: true,
+        }}
+        onSubmit={(gameRound) => addGameRound(gameRound, setGameRounds)}
+      />
     </>
   );
 };

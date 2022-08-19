c480f03d61319f846868e3014270d61006ef16fd
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2022-08-19 12:38:04 +0000

    Anmeldestop

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -144,6 +144,36 @@ function MyApp({ Component, pageProps }: AppProps) {
       </Layout>
     );
   }
+  return (
+    <Layout>
+      <>
+        <h1>Anmeldung geschlossen</h1>
+        <p>
+          Weitere Anmeldungen für die Rollenspieltage 2022 sind nicht mehr
+          möglich.
+        </p>
+        <h2>Ich möchte gerne spontan teilnehmen</h2>
+        <p>
+          Wie bereits in vergangenen Jahren halten wir ein paar Plätze frei,
+          damit spontane Besucher ebenfalls teilnehmen können.
+        </p>
+        <p>
+          Hast du Fragen, dann nimm bitte mit uns
+          <a href="https://rollenspieltage.ch/kontakt/">Kontakt</a> auf.
+        </p>
+        <h2>Ich habe mich angemeldet</h2>
+        <p>
+          Du wirst dein Programm per E-Mail bis am Samstag, 20. August 2022
+          erhalten.
+        </p>
+        <p>
+          Solltest du bis zu diesem Zeitpunkt keine E-Mail erhalten haben, dann
+          melde dich bitte umgehend bei uns per{" "}
+          <a href="https://rollenspieltage.ch/kontakt/">Kontaktformular</a>.
+        </p>
+      </>
+    </Layout>
+  );
 
   if (!isIdentified) {
     return (

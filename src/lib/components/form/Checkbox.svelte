0fb5640221f7dc01e10984f027cc27c34a29b142
Oliver Bucher <oliver.bucher@getabstract.com>
2023-06-05 20:01:54 +0000

    Add Checkbox component

---
+++ src/lib/components/form/Checkbox.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import FormFieldError from './FormFieldError.svelte';
+
+	export let state: boolean;
+	export let error: { condition: () => boolean; message: string } | undefined = undefined;
+</script>
+
+<label class="input-checkbox">
+	<input type="checkbox" bind:checked={state} />
+	<slot />
+</label>
+
+{#if error !== undefined && error.condition()}
+	<FormFieldError>{error.message}</FormFieldError>
+{/if}
--- src/lib/components/main/ContactPage.svelte
+++ src/lib/components/main/ContactPage.svelte
@@ -3,6 +3,7 @@
 	import type { Writable } from 'svelte/store';
 	import TextInput from '../form/TextInput.svelte';
 	import { isNonEmptyString } from '../form/Validation';
+	import Checkbox from '../form/Checkbox.svelte';
 
 	export let appState: Writable<AppState>;
 </script>
@@ -31,6 +32,11 @@
 			message: '"E-Mail" darf nicht leer sein.',
 		}}
 	/>
+
+	<h4>Helfen</h4>
+	<Checkbox bind:state={$appState.wants_to_help}>
+		Ich bin bereit bei einer Spiellücke maximal 2 Stunden an der Kiosk-Kasse auszuhelfen.
+	</Checkbox>
 </div>
 
 <style>
